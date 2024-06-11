59702a5547e6a11f6594c1f90fbd4253200044bc
Sufian5642 <96968832+Sufian5642@users.noreply.github.com>
2024-06-11 10:20:26 +0500

    Add files via upload

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -9,26 +9,15 @@ let dbInstance = null;
 const dbName = "giftdb";
 
 async function connectToDatabase() {
-    if (dbInstance) {
-        return dbInstance;
+    if (dbInstance){
+        return dbInstance
     };
 
-    const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+    const client = new MongoClient(url);
 
-    try {
-        // Task 1: Connect to MongoDB
-        await client.connect();
-
-        // Task 2: Connect to database giftDB and store in variable dbInstance
-        dbInstance = client.db(dbName);
-
-        // Task 3: Return database instance
-        return dbInstance;
-    } catch (error) {
-        console.error('Failed to connect to the database:', error);
-        throw error;
-    }
+    await client.connect();
+    dbInstance = client.db(dbName);
+    return dbInstance;
 }
 
 module.exports = connectToDatabase;
-
